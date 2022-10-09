282ea462ea50f1dccc35d08e3ffd9a28b7984de2
Jenny Berg <jennyberg@Jennys-MBP.lan>
2022-10-09 20:51:41 +0300

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,12 +1,41 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, {useState} from 'react';
+import { StyleSheet, Text, View, Button, FlatList } from 'react-native';
+import * as Contacts from 'expo-contacts';
 
 export default function App() {
+
+  const [contacts, setContacts] = useState([]);
+
+  const getContacts = async () => {
+    const { status } = await Contacts.requestPermissionsAsync();
+
+    if (status === 'granted') {
+      const { data } = await Contacts.getContactsAsync(
+        { fields: [Contacts.Fields.PhoneNumbers]}
+        )
+
+      if (data.length > 0) {
+        setContacts(data);
+        console.log(data);
+      }
+    }
+  }
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <View style={styles.container} >
+      <FlatList
+        style={{marginLeft : '5%', marginTop: '20%'}}
+        keyExtractor={(item, index) => index.toString()}
+        renderItem={({item}) => (
+          <View>
+            <Text>{item.name}</Text>
+            <Text>{item.phoneNumbers[0].number}</Text>
+          </View>
+        )}
+        data={contacts}
+      />
+      <Button title="Get contacts" onPress={getContacts} />
+    </View> 
   );
 }
 
